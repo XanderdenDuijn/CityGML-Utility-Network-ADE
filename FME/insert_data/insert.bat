dded68c0903dcdf9f82ffa35996d49798d9380a0
DESKTOP-4BRJN43\Xander <xdenduijn@gmail.com>
2017-11-02 18:05:39 +0100

    some minor changes

--- FME/insert_data/insert.bat
+++ FME/insert_data/insert.bat
@@ -7,37 +7,46 @@ pushd %~dp0..
 :: step 1
 fme insert_data\workbenches\cityobject.fmw --SourceDataset_CITYGML create_citygml/sewer.gml
 
+pause
+
 :: step 2
 fme insert_data\workbenches\city_furniture.fmw --SourceDataset_CITYGML create_citygml/sewer.gml
+
+pause
 
 :: step 3
 fme insert_data\workbenches\utn9_network.fmw --SourceDataset_CITYGML create_citygml/sewer.gml
 
+pause
+
 :: step 4
 fme insert_data\workbenches\utn9_network_graph.fmw --SourceDataset_CITYGML create_citygml/sewer.gml
+
+pause
 
 :: step 5
 fme insert_data\workbenches\utn9_network_feature.fmw --SourceDataset_CITYGML create_citygml/sewer.gml
 
+pause
+
 :: step 6
 fme insert_data\workbenches\utn9_feature_graph.fmw --SourceDataset_CITYGML create_citygml/sewer.gml
+
+pause
 
 :: step 7
 fme insert_data\workbenches\utn9_node.fmw --SourceDataset_CITYGML create_citygml/sewer.gml
 
+pause
+
 :: step 8
 fme insert_data\workbenches\utn9_link.fmw --SourceDataset_CITYGML create_citygml/sewer.gml
+
 
 :: go back
 popd
 
-\echo
-\echo '***********************************************************************'
-\echo
-\echo 'Done. Check .log files in the folder 'workbenches' for more information'
-\echo
-\echo '***********************************************************************'
-\echo
+pause
 
 
 
